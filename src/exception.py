f45fe277606f46fb0ce4082b88570536bce3991b
RajDwivedi03 <rajdwivedi4814@gmail.com>
2025-03-10 01:50:25 +0530

    LOGGING EXCEPTION

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()  # Gets traceback object
@@ -16,4 +17,8 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message  # Print the detailed error when exception is raised
 
-# Example Usage:
+
+  
+
+
+   
