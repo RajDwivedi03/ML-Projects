51d11b809bdd5ce4eac662762ba53d2536082f32
RajDwivedi03 <rajdwivedi4814@gmail.com>
2025-03-12 12:10:33 +0530

    MODEL TRAINER

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 import dill
+from sklearn.metrics import r2_score
 
 
 def save_object(file_path, obj):
@@ -14,6 +15,52 @@ def save_object(file_path, obj):
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj,file_obj)
 
-
     except Exception as e:
-        raise  CustomException(e,sys)       
+        raise  CustomException(e,sys)  
+
+
+
+'''
+def evaluate_models(x_train, y_train,x_test,y_test,models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+           
+
+          
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report        
+    
+'''
+def evaluate_models(x_train, y_train, x_test, y_test, models):
+    try:
+        report = {}  # Default value of 0.0 if key not found
+
+        for name, model in models.items():
+            model.fit(x_train, y_train)
+            y_train_pred = model.predict(x_train)
+
+            y_test_pred = model.predict(x_test)
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[name] = test_model_score
+
+        return report
+    except Exception as e:
+     raise  CustomException(e,sys) 
