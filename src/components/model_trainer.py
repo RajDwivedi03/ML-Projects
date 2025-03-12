7c8b18bd9971486d214c50a497073020fa2cd187
RajDwivedi03 <rajdwivedi4814@gmail.com>
2025-03-12 17:11:17 +0530

    TRAINED WEB

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -19,23 +19,18 @@ from src.logger import logging
 
 from src.utils import save_object,evaluate_models
 
-
 @dataclass
 class ModelTrainerConfig:
-     trained_model_file_path=os.path.join('artifacts', 'model.pkl')
-
-
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
 
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config = ModelTrainerConfig()
+        self.model_trainer_config=ModelTrainerConfig()
 
 
     def initiate_model_trainer(self,train_array,test_array):
-        logging.info('Verifying model trainer configuration...')
         try:
-            logging.info('Initiating model training process...')  
-
+            logging.info("Split training and test input data")
             x_train,y_train,x_test,y_test=(
                 train_array[:,:-1],
                 train_array[:,-1],
@@ -89,8 +84,10 @@ class ModelTrainer:
                 
             }
 
-            model_report: dict=evaluate_models(x_train=x_train, y_train=y_train,x_test=x_test,y_test=y_test,models=models,param=params)
-                        ## To get best model score from dict
+            model_report:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,
+                                             models=models,param=params)
+            
+            ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
             ## To get best model name from dict
@@ -98,20 +95,25 @@ class ModelTrainer:
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-            best_model=models[best_model_name]
+            best_model = models[best_model_name]
 
             if best_model_score<0.6:
-                raise CustomException("No Best model found")
-            logging.info("BEST FOUND MODEL ON BOTH TRAIN AND TEST DATASET")
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
             )
-            predicted=best_model.predict(x_test)
-            R2_score=r2_score(y_test,predicted)
-            return R2_score
 
+            predicted=best_model.predict(x_test)
+
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+            
+
+
+
+            
         except Exception as e:
-           pass
-             
+            raise CustomException(e,sys)
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -14,7 +15,7 @@ def save_object(file_path, obj):
         os.makedirs(dir_path,exist_ok=True)
 
         with open(file_path, 'wb') as file_obj:
-            dill.dump(obj,file_obj)
+            pickle.dump(obj,file_obj)
 
     except Exception as e:
         raise  CustomException(e,sys)  
@@ -76,6 +77,13 @@ def evaluate_models(x_train, y_train, x_test, y_test, models, param):
             report[name] = test_model_score  # ✅ Store results
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
